9e1080f61c41b55f29aa98910b8c41f06974e9b0
Isaac Ponce <iponced@almaceneslaganga.com>
2026-06-15 10:57:05 -0500

    actulizacion de net, version 1.0.5

--- com.laganga.app/com.laganga.app/MauiProgram.cs
+++ com.laganga.app/com.laganga.app/MauiProgram.cs
@@ -26,21 +26,11 @@ public static class MauiProgram
         builder.Services.AddMauiBlazorWebView();
         builder.Services.AddApplicationShared();
         
-
-        string authDomain = "https://auth.laganga.com";
-        //string apiBaseUrl = "https://api.laganga.com";
-
-        
+  
 
 #if DEBUG
         builder.Services.AddBlazorWebViewDeveloperTools();
-        builder.Logging.AddDebug();
-
-        authDomain = "https://auth-stg.laganga.com";
-        //apiBaseUrl = "https://api-stg.laganga.com/";
-
-
-        
+        builder.Logging.AddDebug();     
 
         AppDomain.CurrentDomain.UnhandledException += (sender, e) =>
         {
@@ -56,82 +46,6 @@ public static class MauiProgram
 #endif
 
         builder.Services.AddSingleton<IFormFactor, FormFactor>();
-
-        // Add device-specific services used by the com.laganga.app.Shared project
-        //builder.Services.AddSingleton<Duende.IdentityModel.OidcClient.Browser.IBrowser, MauiAuthenticationBrowser>();
-        //builder.Services.AddSingleton<IAuthenticationService, AuthenticationService>();
-
-
-
-        // Handler's DI
-        //builder.Services.AddTransient<TokenAuthorizationMessageHandler>();
-        //builder.Services.AddTransient<ResilientApiHandler>();
-
-        //// HttpClient nombrado (reutilizable y eficiente)
-        //builder.Services.AddHttpClient("Api", c =>
-        //{
-        //    c.BaseAddress = new Uri(apiBaseUrl);
-        //    c.Timeout = TimeSpan.FromSeconds(40);
-        //    c.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
-        //    c.DefaultRequestHeaders.Add("User-Agent", "com.laganga.app");
-        //})
-        //.ConfigurePrimaryHttpMessageHandler(() => new SocketsHttpHandler
-        //{
-        //    SslOptions = new System.Net.Security.SslClientAuthenticationOptions
-        //    {
-        //        EnabledSslProtocols = SslProtocols.Tls13 | SslProtocols.Tls12,
-        //        RemoteCertificateValidationCallback = (sender, cert, chain, errors) => true
-        //    }
-        //})
-        //.AddHttpMessageHandler<TokenAuthorizationMessageHandler>()
-        //.AddHttpMessageHandler<ResilientApiHandler>()
-        //.AddPolicyHandler(GetRetryPolicy())
-        //.AddPolicyHandler(GetCircuitBreakerPolicy());
-
-        //builder.Services.AddHttpClient<IApiClient, ApiClient>(c =>
-        //{
-        //    c.BaseAddress = new Uri(apiBaseUrl);
-        //    c.Timeout = TimeSpan.FromSeconds(40);
-        //    c.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
-        //    c.DefaultRequestHeaders.Add("User-Agent", "com.laganga.app");
-        //})
-        //.ConfigurePrimaryHttpMessageHandler(() => new SocketsHttpHandler
-        //{
-        //    SslOptions = new System.Net.Security.SslClientAuthenticationOptions
-        //    {
-        //        EnabledSslProtocols = SslProtocols.Tls13 | SslProtocols.Tls12,
-        //        RemoteCertificateValidationCallback = (sender, cert, chain, errors) => true
-        //    }
-        //})
-        //.AddHttpMessageHandler<TokenAuthorizationMessageHandler>()
-        //.AddHttpMessageHandler<ResilientApiHandler>()
-        //.AddPolicyHandler(GetRetryPolicy())
-        //.AddPolicyHandler(GetCircuitBreakerPolicy());
-
-
-        // Cliente OIDC
-        //builder.Services.AddSingleton(provider =>
-        //{
-        //    var browser = provider.GetRequiredService<Duende.IdentityModel.OidcClient.Browser.IBrowser>();
-        //    return new OidcClient(new OidcClientOptions
-        //    {
-        //        Authority = authDomain,
-        //        ClientId = "71cf1cb3-1803-49d3-b26b-d81baa6296be",
-        //        ClientSecret = "N2NmYzJlZTAtY2E3Mi00Y2I4LTg3YjItY2E0Y2EwMDAwMDAw",
-        //        Scope = "api.laganga.read api.laganga.write offline_access openid profile",
-        //        RedirectUri = "laganga://callback",
-        //        PostLogoutRedirectUri = "laganga://callback",
-        //        Browser = browser,
-        //        BackchannelHandler = new SocketsHttpHandler
-        //        {
-        //            SslOptions = new System.Net.Security.SslClientAuthenticationOptions
-        //            {
-        //                EnabledSslProtocols = SslProtocols.Tls13 | SslProtocols.Tls12,
-        //                RemoteCertificateValidationCallback = (sender, cert, chain, errors) => true
-        //            }
-        //        }
-        //    });
-        //});
 
         return builder.Build();
     }
